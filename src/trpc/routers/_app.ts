15242a653870fc45779f16c80a8b273bcb345dad
samartha935 <147503219+samartha935@users.noreply.github.com>
2026-06-09 17:04:25 +0530

    cleaning & reorganising trpc routes

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,55 +1,8 @@
-import { createTRPCRouter, protectedProcedure } from "../init";
-import { db } from "@/db";
-import { inngest } from "@/inngest/client";
-import z from "zod";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/router";
 
 export const appRouter = createTRPCRouter({
-  getWorkflows: protectedProcedure.query(async ({ ctx }) => {
-    const workflow = await db.query.workflow.findMany();
-
-    return workflow;
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    const newWorkflow = await inngest.send({
-      name: "app/create.workflow",
-      data: {
-        name: "new Workflow using Inngest.",
-      },
-    });
-
-    console.log(newWorkflow, "inngest event triggered. ");
-
-    return newWorkflow;
-  }),
-
-  deleteWorkflow: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .mutation(async (opts) => {
-      const deletedWorkflow = await inngest.send({
-        name: "app/delete.workflow",
-        data: {
-          id: opts.input.id,
-        },
-      });
-
-      console.log(deletedWorkflow, "deleted workflwoooooooo");
-
-      return deletedWorkflow;
-    }),
-
-  getAIResponse: protectedProcedure
-    .input(z.object({ prompt: z.string() }))
-    .mutation(async (opts) => {
-      const AIResponse = await inngest.send({
-        name: "app/AI.request",
-        data: {
-          prompt: opts.input.prompt,
-        },
-      });
-
-      return AIResponse;
-    }),
+  workflows: workflowsRouter,
 });
 
 export type AppRouter = typeof appRouter;
